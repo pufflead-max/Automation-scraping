3c1ccd305cc95b64c18c791ab49f632c3765bc8f
Abeer-kamalaldeen <95304276+Abeer-kamalaldeen@users.noreply.github.com>
2026-01-01 03:48:56 +0200

    adding fourth pipeline

---
+++ airflow/dags/test3.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+with DAG(
+    dag_id="test3",
+    start_date=datetime(2025, 1, 1),
+    schedule=None,
+    catchup=False,
+    tags=["scraping"],
+) as dag:
+
+    BashOperator(
+        task_id="test3",
+        bash_command="python /opt/airflow/scraper/src/main.py",
+        env={
+            "MONGO_DB": "scraping",
+            "JOB_NAME": "airflow_smoke",
+            "MONGO_URI": "mongodb://scraper:ScraperPasswordHere@mongo:27017/scraping?authSource=scraping",
+        },
+    )
